998e9b0cb8e9c30112d618dd3c1abd3e7230bc08
Philip Ryazankin <yaltaphil@gmail.com>
2025-01-08 17:31:34 +0300

    add solution for module 05 task 01 - crud-tasks

--- 05-db/01-crud-tasks/tasks/tasks.controller.ts
+++ 05-db/01-crud-tasks/tasks/tasks.controller.ts
@@ -1,22 +1,62 @@
-import { Controller, Get, Post, Patch, Delete } from "@nestjs/common";
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe
+} from "@nestjs/common";
 import { TasksService } from "./tasks.service";
+import { Task } from "./entities/task.entity";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { UpdateTaskDto } from "./dto/update-task.dto";
 
 @Controller("tasks")
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
-  @Post()
-  create() {}
-
   @Get()
-  findAll() {}
+  @UsePipes(new ValidationPipe({ transform: true }))
+  findAll(
+    @Query("status") status?: string,
+    @Query("page") page?: number,
+    @Query("limit") limit?: number,
+    @Query("sortBy") sortBy?: string,
+  ): Promise<Task[]> {
+
+    if (page < 0 || limit < 0) {
+      throw new BadRequestException();
+    }
+
+    if (sortBy && !['id',"title"].includes(sortBy)) {
+      throw new BadRequestException();
+    }
+
+    return this.tasksService.findAll(status, page, limit, sortBy);
+  }
 
   @Get(":id")
-  findOne() {}
+  findOne(@Param("id") id: number): Promise<Task> {
+    return this.tasksService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() task: CreateTaskDto): Promise<Task> {
+    return this.tasksService.create(task);
+  }
 
   @Patch(":id")
-  update() {}
+  update(@Param("id") id: number, @Body() task: UpdateTaskDto): Promise<Task> {
+    return this.tasksService.update(id, task);
+  }
 
   @Delete(":id")
-  remove() {}
+  remove(@Param("id") id: number): Promise<void> {
+    return this.tasksService.remove(id);
+  }
 }
